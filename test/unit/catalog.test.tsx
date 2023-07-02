de528f64dc5df31edd46946e0232940f9522055b
Anastasia Telnova <telnova.anastasia@gmail.com>
2023-07-02 20:56:20 +0300

    add some units

--- test/unit/catalog.test.tsx
+++ test/unit/catalog.test.tsx
@@ -1,5 +1,5 @@
 import {it, expect} from '@jest/globals'
-import { render, waitFor } from '@testing-library/react'
+import { render, waitFor, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { initStore } from '../../src/client/store'
@@ -10,6 +10,8 @@ import { productsLoaded } from '../../src/client/store'
 import { CartState, CheckoutFormData, Product, ProductShortInfo } from '../../src/common/types'
 import { ProductItem } from '../../src/client/components/ProductItem'
 import { ProductDetails } from '../../src/client/components/ProductDetails'
+import events from '@testing-library/user-event'
+import { Cart } from '../../src/client/pages/Cart'
 
 const basename = '/hw/store';
 const cart = new CartApi();
@@ -50,7 +52,7 @@ const api = new ExampleApi(basename)
 /* @ts-ignore */
 const store = initStore(api, cart);
 
-it ('Проверка полученных товаров в каталоге', async () => {
+it ('в каталоге должны отображаться товары с сервера', async () => {
     const products = await api.getProducts()
     
     store.dispatch(productsLoaded(products.data))
@@ -71,7 +73,7 @@ it ('Проверка полученных товаров в каталоге', 
     await waitFor(() => { expect(getAllByTestId(/[0-1]/i)).toBeDefined()})
 })
 
-it('проверка названия, цены и ссылки на инфо', async () => {
+it('для каждого товара есть название, цена и ссылка на инфо', async () => {
   const products = await api.getProducts()
     
   store.dispatch(productsLoaded(products.data))
@@ -87,33 +89,78 @@ it('проверка названия, цены и ссылки на инфо', 
   )
   const {container} = render(application)
 
-expect(container.querySelector('.card-title')?.textContent).toBeDefined()
-expect(container.querySelector('.card-text')?.textContent).toBeDefined()
+expect(container.querySelector('.card-title')?.textContent).toBe('Sleek Bike')
+expect(container.querySelector('.card-text')?.textContent).toBe('$781')
 expect(container.querySelector('.card-link')?.getAttribute('href')).toBe('/hw/store/catalog/0')
 })
 
 it('На странице с инфо есть название, цена, материал, цвет, кнопка добавления в корзину', async () => {
   const product = await api.getProductById(0)
-    console.log(product);
     
-  // store.dispatch(productsLoaded(products.data))
-
   const application = (
     <BrowserRouter basename={basename}>
       <Provider store={store}>
-      
         <ProductDetails product={product.data} />
-      
       </Provider>
     </BrowserRouter>
   )
 
   const { container } = render(application)
  
-  expect(container.querySelector('.ProductDetails-Name')?.textContent).toBeDefined()
-  expect(container.querySelector('.ProductDetails-Description')?.textContent).toBeDefined()
-  expect(container.querySelector('.ProductDetails-Price')?.textContent).toBeDefined()
-  expect(container.querySelector('.ProductDetails-Color')?.textContent).toBeDefined()
-  expect(container.querySelector('.ProductDetails-Material')?.textContent).toBeDefined()
+  expect(container.querySelector('.ProductDetails-Name')?.textContent).toBe('Sleek Bike')
+  expect(container.querySelector('.ProductDetails-Description')?.textContent).toBe('test')
+  expect(container.querySelector('.ProductDetails-Price')?.textContent).toBe('$781')
+  expect(container.querySelector('.ProductDetails-Color')?.textContent).toBe('cyan')
+  expect(container.querySelector('.ProductDetails-Material')?.textContent).toBe('Rubber')
 
 }) 
+
+it('если товар  добавлен в корзину, в каталоге и на деталке есть сообщение об этом', async() => {
+  const product = await api.getProductById(0)
+  
+  const application = (
+    <BrowserRouter basename={basename}>
+      <Provider store={store}>
+        <ProductDetails product={product.data} />
+        <Catalog />
+      </Provider>
+    </BrowserRouter>
+  )
+
+  const { container, getAllByTestId } = render(application)
+ 
+  await waitFor(() => { expect(getAllByTestId(0)).toBeDefined()})
+  /* @ts-ignore */
+  let cartButton: Element = container.querySelector('.ProductDetails-AddToCart')
+
+  await events.click(cartButton)
+  
+  expect(container.querySelector('p .CartBadge')?.textContent).toBe('Item in cart')
+  expect(container.querySelector('.card-body .CartBadge')?.textContent).toBe('Item in cart')
+
+})
+
+it('если товар  добавлен в корзину, повторное нажатие увеличит его количество', async() => {
+  const product = await api.getProductById(0)
+  
+  const application = (
+    <BrowserRouter basename={basename}>
+      <Provider store={store}>
+        <Cart />
+        <ProductDetails product={product.data} />
+      </Provider>
+    </BrowserRouter>
+  )
+
+  const { container, getAllByTestId } = render(application)
+ 
+  await waitFor(() => { expect(getAllByTestId(0)).toBeDefined()})
+  expect(container.querySelector('.Cart-Count')?.textContent).toBe('1')
+  /* @ts-ignore */
+  let cartButton: Element = container.querySelector('.ProductDetails-AddToCart')
+  await events.click(cartButton)
+
+  expect(container.querySelector('.Cart-Count')?.textContent).toBe('2')
+  
+
+})
